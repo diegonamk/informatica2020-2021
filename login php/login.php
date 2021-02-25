ac99e7867f8e77d360e0fb1e1aaac6bef06e8282
diegonamk <d.namkhai@studenti.gobettivolta.gov.it>
2021-02-26 00:51:44 +0100

    Funziona
    fanculo 000webhost

--- login php/login.php
+++ login php/login.php
@@ -1,15 +1,11 @@
 <html>
     <head>
+    <?php ob_start(); ?>
         <Title>Login Benvenuto</Title>
     </head>
 
     <body>
 
-
-        <?php
-        
-
-        ?>
         <?php
             $servername = "localhost";
             $username = "id15980825_diego";
@@ -22,21 +18,25 @@
             if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
             }
-            $name = $_POST["name"];
-            $password = $_POST["password"];
+            //if ($_POST['username']=="Diego") echo "<script>alert(\"si/\")</script>";
+            //else echo "<script>alert(\"no/\")</script>";
+
+            $name = $_POST['username'];
+            $password = $_POST['password'];
+            //if ($name=="Diego") echo "<script>alert(\"si/\")</script>";
 
             $sql = "SELECT * FROM user WHERE password='$password' AND name='$name'";
             $result = $conn->query($sql);
-
-            if (isset($result)) {
-                echo "<h1>Benvenuto" . $name ." </h1>";
+            $row = $result->fetch_assoc();
+            $conn->close();
+            if (isset($row['name'])) {
+                echo "<h1>Benvenuto " . $name." </h1>";
             }
             else 
             {
                 echo "<script>alert(\"nome utente o password sbagliata/\")</script>";
-                header("Location: /log.html");
             }
-            $conn->close();
+            
 
 
 
