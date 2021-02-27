bf5a1b482cb5be1b0d25df8f3d0949ec279a4cb6
diegonamk <d.namkhai@studenti.gobettivolta.gov.it>
2021-02-28 00:48:38 +0100

    q6 incompleta

--- query 10 php/query.php
+++ query 10 php/query.php
@@ -15,8 +15,7 @@
     //query1
     //Nome Cognome dei Fornitori di Xa(citta)
     //Q1Xa
-    //$sql = "SELECT nome, cognome FROM fornitore WHERE citta='".$_POST['Q1Xa']."'";
-    $sql = "SELECT nome, cognome FROM fornitore WHERE citta='firenze'";
+    $sql = "SELECT nome, cognome FROM fornitore WHERE citta='".$_POST['Q1Xa']."'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -38,16 +37,100 @@
     //Codice delle Merci fornite da Fornitore di Xa(citta)
     //Q2Xa
     $sql = "SELECT codForn, merce, fornitore FROM fornitore,fornitura WHERE citta='".$_POST['Q2Xa']."' AND fornitore = codForn ";
-    //$sql = "SELECT nome, cognome FROM fornitore WHERE citta='firenze'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        echo "Codice delle Merci fornite da Fornitore di ".$_POST['Q2Xa'];
+        echo "Q2 Codice delle Merci fornite da Fornitore di ".$_POST['Q2Xa'];
         echo "<br>";
         echo "------------------------------------------------------------------------";
         while($row = $result->fetch_assoc()) {  
             echo "<br>";
             echo "codice merce:" . $row["merce"];
+        }
+        echo "<br>";
+        echo "------------------------------------------------------------------------";
+        echo "<br>";
+    } else {
+                echo "0 results";
+    }
+
+    //query3
+    //Nome Cognome dei Fornitori che forniscono Merce di Colore Xa(colore scelto)
+    //Q3Xa
+    $sql = "SELECT nome, cognome, codForn, fornitore, colore, codMerce, merce FROM fornitore,fornitura,merce WHERE colore='".$_POST['Q3Xa']."' AND fornitore = codForn AND merce = codMerce ";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "Q3 Nome Cognome dei Fornitori che forniscono Merce di Colore ".$_POST['Q3Xa'];
+        echo "<br>";
+        echo "------------------------------------------------------------------------";
+        while($row = $result->fetch_assoc()) {  
+            echo "<br>";
+            echo "nome:" . $row["nome"]." cognome:" . $row["cognome"];
+        }
+        echo "<br>";
+        echo "------------------------------------------------------------------------";
+        echo "<br>";
+    } else {
+                echo "0 results";
+    }
+
+    //query4
+    //Nome Cognome dei Fornitoriche che forniscono Merce di Colore Xa(colore scelto 1) oppure di Colore Xb(colore scelto 2)
+    //Q4Xa
+    //Q4Xb
+    $sql = "SELECT nome, cognome, codForn, fornitore, colore, codMerce, merce 
+            FROM fornitore,fornitura,merce 
+            WHERE (colore='".$_POST['Q4Xa']."' OR colore='".$_POST['Q4Xb']."') AND fornitore = codForn AND merce = codMerce ";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "Q4 Nome Cognome dei Fornitoriche forniscono Merce di Colore ".$_POST['Q4Xa']." oppure di Colore ".$_POST['Q4Xb'];
+        echo "<br>";
+        echo "------------------------------------------------------------------------";
+        while($row = $result->fetch_assoc()) {  
+            echo "<br>";
+            echo "nome:" . $row["nome"]." cognome:" . $row["cognome"];
+        }
+        echo "<br>";
+        echo "------------------------------------------------------------------------";
+        echo "<br>";
+    } else {
+                echo "0 results";
+    }
+
+    //query5
+    //Fornitori che forniscono almeno una Merce
+    $sql = "SELECT fornitore FROM fornitura";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "Q5 Fornitori che forniscono almeno una Merce";
+        echo "<br>";
+        echo "------------------------------------------------------------------------";
+        while($row = $result->fetch_assoc()) {  
+            echo "<br>";
+            echo "Fornitore:" . $row["fornitore"];
+        }
+        echo "<br>";
+        echo "------------------------------------------------------------------------";
+        echo "<br>";
+    } else {
+                echo "0 results";
+    }
+
+    //query6
+    //Nome e Cognome dei Fornitori che forniscono almeno 2 Merci
+    $sql = "SELECT codForn, nome, cognome, fornitore, merce, f1.fornitore, f1.merceFROM fornitore, fornitura, fornitura AS f1 WHERE codForn=(fornitore = (fornitore=f1.fornitore AND merce<>f1.merce))";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "Q6 Nome e Cognome dei Fornitori che forniscono almeno 2 Merci";
+        echo "<br>";
+        echo "------------------------------------------------------------------------";
+        while($row = $result->fetch_assoc()) {  
+            echo "<br>";
+            echo "nome:" . $row["nome"]." cognome:" . $row["cognome"];
         }
         echo "<br>";
         echo "------------------------------------------------------------------------";
