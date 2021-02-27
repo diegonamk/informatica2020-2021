a4781b1b96f8cf9f0db538c2cfe082195096bdb0
diegonamk <d.namkhai@studenti.gobettivolta.gov.it>
2021-02-27 22:40:01 +0100

    Q1

--- query 10 php/query.php
+++ query 10 php/query.php
@@ -1,2 +1,29 @@
 <?php
-$_POST
+$servername = "localhost";
+$username = "id15980825_diego";
+$password = "3w7|?{[GH_d>WVVn";
+$dbname = "id15980825_logistica";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+//query1
+//Nome Cognome dei Fornitori di Xa(citta)
+//Q1Xa
+$sql = "SELECT nome, cognome FROM fornitore WHERE citta='".$_POST['Q1Xa']."'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo "Q1 Nome Cognome dei Fornitori di ".$_POST['Q1Xa'];
+    echo "<br>";
+    echo "nome " . $row["nome"]. " cognome " . $row["cognome"];
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
